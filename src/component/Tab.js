5b7dde358e52c1d0dde92ec38426a7c1bc54cb83
Azzy Kesuma <official.azzy@gmail.com>
2021-07-07 09:54:49 +0700

    first commit

---
+++ src/component/Tab.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Tab.css'
+
+function Tab() {
+    return (
+        <div className='MenuMain'>
+            <div className='triangle'></div>
+            <div className='tabContainer'>
+                <ul className='NavMenu'>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Tab
